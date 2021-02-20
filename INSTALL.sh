bca3753870239b67f2c24f4e05df350577b40edf
alejandromume <alejandromume80@gmail.com>
2021-02-20 20:38:09 +0000

    fixed installer

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,23 +1,24 @@
-#cp -r ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/ /usr/share/
 RED='\033[0;31m'
 Green='\033[1;32m'
 ORANGE='\033[1;33m'
 BOLD_CYAN='\033[1;36m'
 NC='\033[0m' # No Color
 
-printf "${BOLD_CYAN}\n\n[-] ${NC}Installing Theme...\n"
-sudo cp ubunly.zsh-theme $ZSH_CUSTOM/themes
+zsh_custom_directory="/home/$USER/.oh-my-zsh/custom"
 
-if [ ! -d "/usr/share/zsh-autosuggestions" ] || [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
-	git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions --quiet	
-	sudo cp -r $ZSH_CUSTOM/plugins/zsh-autosuggestions /usr/share/
+sudo cp ubunly.zsh-theme $zsh_custom_directory/themes
+printf "${BOLD_CYAN}\n\n[-] ${NC}Installing Theme...\n"
+
+if [ ! -d "/usr/share/zsh-autosuggestions" ] || [ ! -d "$zsh_custom_directory/plugins/zsh-autosuggestions" ]; then
+	git clone https://github.com/zsh-users/zsh-autosuggestions.git $zsh_custom_directory/plugins/zsh-autosuggestions --quiet
+	sudo cp -r $zsh_custom_directory/plugins/zsh-autosuggestions /usr/share/
 else
 	printf "${RED}[-] ${NC}zsh-autosuggestions is alredy installed\n"
 fi
 
-if [ ! -d "/usr/share/zsh-syntax-highlighting" ] || [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
-	sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting --quiet
-	sudo cp -r $ZSH_CUSTOM/plugins/zsh-syntax-highlighting /usr/share/
+if [ ! -d "/usr/share/zsh-syntax-highlighting" ] || [ ! -d "$zsh_custom_directory/plugins/zsh-syntax-highlighting" ]; then
+	sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $zsh_custom_directory/plugins/zsh-syntax-highlighting --quiet
+	sudo cp -r $zsh_custom_directory/plugins/zsh-syntax-highlighting /usr/share/
 else	
 	printf "${RED}[-] ${NC}zsh-syntax-highlighting is alredy installed\n"
 fi
