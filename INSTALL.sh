0edfad967b54441c0c5fca8553e889150064f5a0
Simon Slamka <51794014+simonSlamka@users.noreply.github.com>
2021-07-18 01:02:14 +0200

    added two of the previously removed sudo
    Since the author is copying to `/usr/share`, we need root's blessing to run cp. Made a mistake previously, I shouldn't have removed these two.

--- INSTALL.sh
+++ INSTALL.sh
@@ -17,14 +17,14 @@ printf "╚═══════════════════════
 
 if [ ! -d "/usr/share/zsh-autosuggestions" ] || [ ! -d "$zsh_custom_directory/plugins/zsh-autosuggestions" ]; then
 	git clone https://github.com/zsh-users/zsh-autosuggestions.git $zsh_custom_directory/plugins/zsh-autosuggestions --quiet
-	cp -r $zsh_custom_directory/plugins/zsh-autosuggestions /usr/share/
+	sudo cp -r $zsh_custom_directory/plugins/zsh-autosuggestions /usr/share/
 else
 	printf "${RED}[-] ${NC}zsh-autosuggestions is alredy installed\n"
 fi
 
 if [ ! -d "/usr/share/zsh-syntax-highlighting" ] || [ ! -d "$zsh_custom_directory/plugins/zsh-syntax-highlighting" ]; then
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $zsh_custom_directory/plugins/zsh-syntax-highlighting --quiet
-	cp -r $zsh_custom_directory/plugins/zsh-syntax-highlighting /usr/share/
+	sudo cp -r $zsh_custom_directory/plugins/zsh-syntax-highlighting /usr/share/
 else	
 	printf "${RED}[-] ${NC}zsh-syntax-highlighting is alredy installed\n"
 fi
