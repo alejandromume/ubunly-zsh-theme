3bd308af4c2232a2726eb73f685e14a859ac4a81
Simon Slamka <51794014+simonSlamka@users.noreply.github.com>
2021-07-17 23:52:45 +0200

    removed sudo and changed /home/$USER/ to $HOME/

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,9 +4,9 @@ ORANGE='\033[1;33m'
 BOLD_CYAN='\033[1;36m'
 NC='\033[0m' # No Color
 
-zsh_custom_directory="/home/$USER/.oh-my-zsh/custom"
+zsh_custom_directory="$HOME/.oh-my-zsh/custom"
 
-sudo cp ubunly.zsh-theme $zsh_custom_directory/themes
+cp ubunly.zsh-theme $zsh_custom_directory/themes
 printf "${BOLD_CYAN}\n\n[-] ${NC}Installing Theme...\n\n"
 
 
@@ -17,14 +17,14 @@ printf "╚═══════════════════════
 
 if [ ! -d "/usr/share/zsh-autosuggestions" ] || [ ! -d "$zsh_custom_directory/plugins/zsh-autosuggestions" ]; then
 	git clone https://github.com/zsh-users/zsh-autosuggestions.git $zsh_custom_directory/plugins/zsh-autosuggestions --quiet
-	sudo cp -r $zsh_custom_directory/plugins/zsh-autosuggestions /usr/share/
+	cp -r $zsh_custom_directory/plugins/zsh-autosuggestions /usr/share/
 else
 	printf "${RED}[-] ${NC}zsh-autosuggestions is alredy installed\n"
 fi
 
 if [ ! -d "/usr/share/zsh-syntax-highlighting" ] || [ ! -d "$zsh_custom_directory/plugins/zsh-syntax-highlighting" ]; then
-	sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $zsh_custom_directory/plugins/zsh-syntax-highlighting --quiet
-	sudo cp -r $zsh_custom_directory/plugins/zsh-syntax-highlighting /usr/share/
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $zsh_custom_directory/plugins/zsh-syntax-highlighting --quiet
+	cp -r $zsh_custom_directory/plugins/zsh-syntax-highlighting /usr/share/
 else	
 	printf "${RED}[-] ${NC}zsh-syntax-highlighting is alredy installed\n"
 fi
